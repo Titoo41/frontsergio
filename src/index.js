ad10277aed77ee85f1982aa9fefba6fef929d1a1
titoo41 <admcoronasdelsur@gmail.com>
2024-10-31 20:50:39 -0300

    configurando nueva app

--- src/index.js
+++ src/index.js
@@ -8,7 +8,7 @@ const root = createRoot(document.getElementById('root'));
 root.render(
 <Auth0Provider
     domain="dev-imgglgh1w6k6yun0.us.auth0.com"
-    clientId="liplau4JuFVOcqFl3vjN6Re4UzV6w1BD"
+    clientId="HHCejbYUFvUo1BUFBNOXfXraFslfxUrP"
     authorizationParams={{
       redirect_uri: window.location.origin
     }}
