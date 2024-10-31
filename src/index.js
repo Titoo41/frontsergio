3a60c3a5bc17af059c3203def79ef0196b1b6264
titoo41 <admcoronasdelsur@gmail.com>
2024-10-31 20:41:50 -0300

    nuevo proyecto en desarrollo

--- src/index.js
+++ src/index.js
@@ -1,20 +1,18 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import "./index.css";
-import App from "./App";
-import { Auth0Provider } from "@auth0/auth0-react";
+import React from 'react';
+import { createRoot } from 'react-dom/client';
+import { Auth0Provider } from '@auth0/auth0-react';
+import App from './App';
 
-const root = ReactDOM.createRoot(document.getElementById("root"));
+const root = createRoot(document.getElementById('root'));
+
 root.render(
-  <React.StrictMode>
-    <Auth0Provider
-      domain="dev-imgglgh1w6k6yun0.us.auth0.com"
-      clientId="OLWzLdKTKlhCgHARpCGBYH4zySMF2Fzh"
-      authorizationParams={{
-        redirect_uri: window.location.origin,
-      }}
-    >
-      <App />
-    </Auth0Provider>
-  </React.StrictMode>
+<Auth0Provider
+    domain="dev-imgglgh1w6k6yun0.us.auth0.com"
+    clientId="liplau4JuFVOcqFl3vjN6Re4UzV6w1BD"
+    authorizationParams={{
+      redirect_uri: window.location.origin
+    }}
+  >
+    <App />
+  </Auth0Provider>,
 );
